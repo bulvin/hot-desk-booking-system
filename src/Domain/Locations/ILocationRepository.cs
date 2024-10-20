8ff2f025201cd0e372d4dcd27a221231b3fa4ca4
Janek <84191665+bulvin@users.noreply.github.com>
2024-10-20 23:05:23 +0200

    remove location if desk not exists (#2)

--- src/Domain/Locations/ILocationRepository.cs
+++ src/Domain/Locations/ILocationRepository.cs
@@ -3,4 +3,6 @@ namespace Domain.Locations;
 public interface ILocationRepository
 {
     void Add(Location location);
+    void Delete(Location location);
+    Task<Location?> GetById(Guid id, CancellationToken cancellationToken = default);
 }
--- src/Web.Api/Controllers/LocationsController.cs
+++ src/Web.Api/Controllers/LocationsController.cs
@@ -1,4 +1,5 @@
 using Application.Locations.Create;
+using Application.Locations.Delete;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -21,5 +22,12 @@ public class LocationsController : ControllerBase
         var response = await _mediator.Send(request);
         return Created($"/locations/{response.Id}", response);
     }
-    
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteLocation(Guid id)
+    {
+        var command = new DeleteLocationCommand(id);
+        await _mediator.Send(command);
+        return NoContent();
+    }
 }
